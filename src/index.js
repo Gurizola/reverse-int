6b6bce1751cf70b3316f7b186c0acb4b589ebefe
Gurizola <aleks.tolstyi@mail.ru>
2022-06-25 16:33:30 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    if(n > 0){
+        let result = '';
+        n = n.toString();
+        for(let i = n.length - 1; i >= 0; i--){
+         result = result + n[i];
+     }
+        return result;
+    }else{
+        let result = '';
+        n = (n * -1).toString();
+        for(let i = n.length - 1; i >= 0; i--){
+            result = result + n[i];
+        }
+        return result;
+     }
 }
